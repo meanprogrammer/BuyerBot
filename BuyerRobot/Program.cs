cf988b93a960d857208f95c95b4b5d087b0ab8bc
meanprogrammer <mean.programmer.d@gmail.com>
2016-09-09 10:24:27 +0800

    1234

--- BuyerRobot/Program.cs
+++ BuyerRobot/Program.cs
@@ -11,12 +11,7 @@ namespace BuyerRobot
     class Program
     {
         static void Main(string[] args)
-        {
-            string URI = "http://www.footlocker.com/catalog/miniAddToCart.cfm";
-            string myParameters = "requestKey=457152ED515j1ABA&qty=1&size=09.0&the_model_nbr=247405&sku=11006601&storeNumber=00000&fulfillmentType=SHIP_FROM_STORE&storeCostOfGoods=0.00"+
-                "&inlineAddToCart=0&coreMetricsCategory=blank&hasXYPromo=false&BV_TrackingTag_Review_Display_Sort=http://footlocker.ugc.bazaarvoice.com/8001/247405/reviews.djs?format=embeddedhtml&BV_TrackingTag_QA_Display_Sort=&rdo_deliveryMethod=shiptohome&inlineAddToCart=1";
-
-          
+        {        
 
         }
     }
